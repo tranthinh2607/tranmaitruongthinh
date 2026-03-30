3fac52d8bae910a6209d56075a80f26412921f17
tranthinh2607 <tranmaitruongthinh2607@gmail.com>
2026-03-30 13:25:10 +0700

    UI mobile responsive fix

--- fix_styles.js
+++ fix_styles.js
@@ -1,20 +1,48 @@
 const fs = require('fs');
-let html = fs.readFileSync('category.html', 'utf8');
+const cheerio = require('cheerio');
 
-// 1. Remove height: 320px block logic
-html = html.replace(/height:\s*\d+px\s*width:\s*100%;/g, 'width: 100%; height: 100%;');
+function fixHtml(file) {
+    if (!fs.existsSync(file)) return;
+    let html = fs.readFileSync(file, 'utf8');
+    const $ = cheerio.load(html, { decodeEntities: false });
 
-// 2. Fix content flex-grow missing semicolon
-html = html.replace(/flex-grow:\s*1\s*width:\s*100%;/g, 'width: 100%; flex-grow: 1;');
+    // 1. Find all article containers
+    $('.blog-post-thumb').each((i, el) => {
+        const parentDiv = $(el).closest('div.d-flex.flex-column.flex-md-row');
+        if (parentDiv.length > 0) {
+            parentDiv.removeClass('d-flex flex-column flex-md-row').addClass('row m-0');
+        }
 
-// 3. Remove duplicate style tags on img 
-html = html.replace(/(<img[^>]*style="[^"]*")\s+style="[^"]*"/g, '$1');
+        // 2. Add Bootstrap col classes to thumb
+        $(el).addClass('col-md-5 col-12');
+        $(el).css('flex-basis', ''); // remove inline
+        $(el).css('max-width', '');
+        $(el).css('min-height', '250px');
+        
+        // Remove 'position-relative' if it exists to let image define height naturally
+        $(el).removeClass('position-relative');
+        
+        // 3. Find the img inside and remove absolute positioning
+        const img = $(el).find('img');
+        if (img.length > 0) {
+            img.css('position', '');
+            img.css('top', '');
+            img.css('left', '');
+            img.css('height', '100%');
+            img.css('width', '100%');
+            img.css('min-height', '250px');
+            img.css('object-fit', 'cover');
+        }
+    });
 
-// 4. Remove flex-md-row from inner content that was accidentally added
-html = html.replace(/class="blog-post-content([^"]*)flex-md-row([^"]*)"/g, 'class="blog-post-content$1$2"');
+    $('.blog-post-content').each((i, el) => {
+        $(el).addClass('col-md-7 col-12');
+        $(el).css('flex-basis', '');
+        $(el).css('max-width', '');
+    });
 
-// 5. Let's make sure the min-height is 200px instead of 250px so it fits well horizontally
-html = html.replace(/min-height:\s*250px/g, 'min-height: 200px');
+    fs.writeFileSync(file, $.html());
+    console.log(`Fix applied to ${file}`);
+}
 
-fs.writeFileSync('category.html', html);
-console.log('Fixed category html styles.');
+['index.html', 'src/index.html', 'category.html'].forEach(fixHtml);
